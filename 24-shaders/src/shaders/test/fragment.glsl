ad6cda680e50d851a827b79faaa1f4c55f07bab7
Sam Thompson <samthompson292@gmail.com>
2021-06-01 19:30:39 +0100

    lesson 24

---
+++ 24-shaders/src/shaders/test/fragment.glsl
@@ -0,0 +1,16 @@
+// precision mediump float;
+
+uniform vec3 uColor;
+uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
+// varying float vRandom;
+
+void main() {
+    // gl_FragColor = vec4(vRandom, vRandom * 0.5, 1.0, 1.0);
+    vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 2.0 + 0.5;
+    gl_FragColor = textureColor;
+    // gl_FragColor = vec4(uColor, 1.0);
+}
