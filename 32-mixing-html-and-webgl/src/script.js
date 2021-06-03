1d3f37339a735e48033b8e6bd51bee2d8f7246c1
Sam Thompson <samthompson292@gmail.com>
2021-06-03 18:20:30 +0100

    lesson 32

--- 32-mixing-html-and-webgl/src/script.js
+++ 32-mixing-html-and-webgl/src/script.js
@@ -8,13 +8,13 @@ import { gsap } from 'gsap'
  * Loaders
  */
 const loadingBarElement = document.querySelector('.loading-bar')
+
+let sceneReady = false
 const loadingManager = new THREE.LoadingManager(
     // Loaded
-    () =>
-    {
+    () => {
         // Wait a little
-        window.setTimeout(() =>
-        {
+        window.setTimeout(() => {
             // Animate overlay
             gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0, delay: 1 })
 
@@ -22,11 +22,14 @@ const loadingManager = new THREE.LoadingManager(
             loadingBarElement.classList.add('ended')
             loadingBarElement.style.transform = ''
         }, 500)
+
+        window.setTimeout(() => {
+            sceneReady = true
+        }, 2000)
     },
 
     // Progress
-    (itemUrl, itemsLoaded, itemsTotal) =>
-    {
+    (itemUrl, itemsLoaded, itemsTotal) => {
         // Calculate the progress and update the loadingBarElement
         const progressRatio = itemsLoaded / itemsTotal
         loadingBarElement.style.transform = `scaleX(${progressRatio})`
@@ -118,8 +121,7 @@ debugObject.envMapIntensity = 5
  */
 gltfLoader.load(
     '/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
-    (gltf) =>
-    {
+    (gltf) => {
         gltf.scene.scale.set(2.5, 2.5, 2.5)
         gltf.scene.rotation.y = Math.PI * 0.5
         scene.add(gltf.scene)
@@ -127,6 +129,25 @@ gltfLoader.load(
         updateAllMaterials()
     }
 )
+
+/**
+ * Points of interest
+ */
+const raycaster = new THREE.Raycaster()
+const points = [
+    {
+        position: new THREE.Vector3(1.55, 0.3, - 0.6),
+        element: document.querySelector('.point-0')
+    },
+    {
+        position: new THREE.Vector3(0.5, 0.8, - 1.6),
+        element: document.querySelector('.point-1')
+    },
+    {
+        position: new THREE.Vector3(1.6, - 1.3, - 0.7),
+        element: document.querySelector('.point-2')
+    }
+]
 
 /**
  * Lights
@@ -147,8 +168,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -193,10 +213,46 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-const tick = () =>
-{
+const tick = () => {
     // Update controls
     controls.update()
+
+    // Update points only when the scene is ready
+    if(sceneReady) {
+        // Go through each point
+        for(const point of points) {
+            // Get 2D screen position
+            const screenPosition = point.position.clone()
+            screenPosition.project(camera)
+    
+            // Set the raycaster
+            raycaster.setFromCamera(screenPosition, camera)
+            const intersects = raycaster.intersectObjects(scene.children, true)
+    
+            // No intersect found
+            if (intersects.length === 0) {
+                // Show
+                point.element.classList.add('visible')
+            } else {
+                // Get the distance of the intersection and the distance of the point
+                const intersectionDistance = intersects[0].distance
+                const pointDistance = point.position.distanceTo(camera.position)
+    
+                // Intersection is close than the point
+                if (intersectionDistance < pointDistance) {
+                    // Hide
+                    point.element.classList.remove('visible')
+                } else {
+                    // Show
+                    point.element.classList.add('visible')
+                }
+            }
+    
+            const translateX = screenPosition.x * sizes.width * 0.5
+            const translateY = - screenPosition.y * sizes.height * 0.5
+            point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`
+        }
+    }
 
     // Render
     renderer.render(scene, camera)
