9610435a9771585ac224f4ec7089f0c5e1213cc9
Sam Thompson <samthompson292@gmail.com>
2024-03-19 16:48:27 +0000

    Halftone shading

---
+++ 37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
@@ -0,0 +1,58 @@
+uniform vec3 uColor;
+uniform vec2 uResolution;
+uniform float uShadowRepetitions;
+uniform vec3 uShadowColor;
+uniform float uLightRepetitions;
+uniform vec3 uLightColor;
+
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include ../includes/ambientLight.glsl
+#include ../includes/directionalLight.glsl
+#include ../includes/halftone.glsl
+
+void main() {
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    vec3 normal = normalize(vNormal);
+    vec3 color = uColor;
+
+    // Lights
+    vec3 light = vec3(0.0);
+    light += ambientLight(vec3(1.0), 1.0);
+    light += directionalLight(
+        vec3(1.0),
+        1.0,
+        normal,
+        vec3(1.0, 1.0, 0.0),
+        viewDirection,
+        1.0
+    );
+    color *= light;
+
+    // Shadow halftone
+    color = halftone(
+        color,
+        uShadowRepetitions,
+        vec3(0.0, -1.0, 0.0),
+        -0.8,
+        1.5,
+        uShadowColor,
+        normal
+    );
+    // Light halftone
+    color = halftone(
+        color,
+        uLightRepetitions,
+        vec3(1.0, 1.0, 0.0),
+        0.5,
+        1.5,
+        uLightColor,
+        normal
+    );
+
+    // Final color
+    gl_FragColor = vec4(color, 1.0);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
